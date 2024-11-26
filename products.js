79cdcbbce866e07c50380631faf51c045afc3d66
Deniz Ister <isterdeniz01@gmail.com>
2024-11-26 15:45:29 +0400

    added files

---
+++ products.js
@@ -0,0 +1,35 @@
+let products = [
+    {
+        "id": 1001,
+        "title":"cat food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresistible</em>,oragnic goodness for your cat.",
+        "price": 20.00,
+        "image": "images/cat-animation.png",
+        "availableInventory":10,
+        "rating": 3
+    },
+    {
+        "id": 1002, 
+        "title":"Yarn",
+        "description":"Yarn your car can play with for a very <strong>long</strong>time!",
+        "price":2.99,
+        "image":"images/istockphoto-1423650559-612x612.jpg",
+        "availableInventory": 7,
+        "rating": 2,
+        
+    },
+    {
+        "id":1003,
+        "title":"Cat litter",
+        "description": "<strong>Premium</strong> cat litter for your cat.",
+        "price":19.00,
+        "image":"images/Cat litter.jpg",
+        "availableInventory": 8,
+        "rating": 4,
+
+
+
+    },
+
+]
+
